b394313ca5aad6a189675a7af38997cd4a6f526f
Ananda Rebecca Gurgel Valeriano <gurgel.ananda@gmail.com>
2021-03-18 17:53:41 -0300

    Anotações das aulas

---
+++ js/main.js
@@ -0,0 +1,60 @@
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Errouuuuuu";
+    elemento.innerHTML = "Errouuuuuu";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Poxa vida hein owww"
+    elemento.innerHTML = "Poxa vida hein owww";
+}
+
+function botao(){
+    alert("Botão funcionando :D");
+}
+
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// function validaIdade(idade){
+//     var validar;
+
+//     if(idade >= 18){
+//         validar = true
+//     }else{
+//         validar = false
+//     }
+//     return validar;
+// }
+
+// var idade = prompt("Qual a sua idade");
+// console.log(validaIdade(idade));
+
+//alert(soma(5,10));
+
+
+// var d = new Date();
+// alert(d.getDay());
+// alert(d.getHours());
+// alert(d.getMinutes());
+
+
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta.nome);
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.reverse());  //retorna a lista de trás para frente 
+
+
+//var nome = "Ananda Gurgel";
+//alert("Bem vinda " + nome);
